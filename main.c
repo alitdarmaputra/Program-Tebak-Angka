64e78bbb25b238752970e292885bd05e5a6956fd
alitdarmaputra <alitdarmaputra@gmail.com>
2021-05-08 00:37:30 +0800

    Menambahkan fungsi rand dan pengkondisian

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main (){
+  time_t t;
   int guess_num, user_num;
+
+  srand((unsigned) time(&t));
   guess_num = rand() % 30;
+  printf("%d\n", guess_num);
   printf("--- Program Tebak Angka ---\n");
   printf("tekan <enter> untuk melanjutkan...\n");
   getch();
@@ -11,6 +15,14 @@ int main (){
   for(int i = 0; i<3; i++){
     printf("Tebakan ke-%d :", i+1);
     scanf("%d", &user_num);
+    if(user_num == guess_num){
+      printf("Tebakkan anda benar\n");
+      break;
+    }else if(user_num > guess_num){
+      printf("Angka yang ditebak kurang dari tebakan anda\n");
+    }else if(user_num < guess_num){
+      printf("Angka yang ditebak lebih dari tebakan anda\n");
+    }
   }
   return 0;
 }
